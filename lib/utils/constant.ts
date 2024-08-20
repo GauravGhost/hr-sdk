d854f5179d6052b174d3a5a61e4ccdafc205fe2e
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-08-20 23:39:16 +0530

    First Commit

---
+++ lib/utils/constant.ts
@@ -0,0 +1,38 @@
+const API_ENDPOINT = "https://webapi.highrise.game";
+const WS_ENDPOINT = "wss://highrise.game/web/webapi";
+
+
+export const constant = {
+    API_ENDPOINT,
+    WS_ENDPOINT,
+
+    AnchorHitRequest: "AnchorHitRequest",
+    BuyItemRequest: "BuyItemRequest",
+    BuyRoomBoostRequest: "BuyRoomBoostRequest",
+    BuyVoiceTimeRequest: "BuyVoiceTimeRequest",
+    ChangeRoomPrivilegeRequest: "ChangeRoomPrivilegeRequest",
+    ChannelRequest: "ChannelRequest",
+    ChatRequest: "ChatRequest",
+    CheckVoiceChatRequest: "CheckVoiceChatRequest",
+    EmoteRequest: "EmoteRequest",
+    FloorHitRequest: "FloorHitRequest",
+    GetBackpackRequest: "GetBackpackRequest",
+    GetConversationsRequest: "GetConversationsRequest",
+    GetInventoryRequest: "GetInventoryRequest",
+    GetMessagesRequest: "GetMessagesRequest",
+    GetRoomPrivilegeRequest: "GetRoomPrivilegeRequest",
+    GetRoomUsersRequest: "GetRoomUsersRequest",
+    GetUserOutfitRequest: "GetUserOutfitRequest",
+    GetWalletRequest: "GetWalletRequest",
+    InviteSpeakerRequest: "InviteSpeakerRequest",
+    KeepaliveRequest: "KeepaliveRequest",
+    LeaveConversationRequest: "LeaveConversationRequest",
+    ModerateRoomRequest: "ModerateRoomRequest",
+    MoveUserToRoomRequest: "MoveUserToRoomRequest",
+    ReactionRequest: "ReactionRequest",
+    RemoveSpeakerRequest: "RemoveSpeakerRequest",
+    SendMessageRequest: "SendMessageRequest",
+    SetOutfitRequest: "SetOutfitRequest",
+    TeleportRequest: "TeleportRequest",
+    TipUserRequest: "TipUserRequest"
+}
